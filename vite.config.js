a9f6a7ab29c6dc411e58d6860a599b02211b0dd8
Christofer Daniel Chaves Pauth <42120690+Christofer2002@users.noreply.github.com>
2024-12-09 17:24:08 -0600

    Update build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sport-management/', // Añade esta línea
   plugins: [react()], // O Vue si estás usando Vue
 });
 
